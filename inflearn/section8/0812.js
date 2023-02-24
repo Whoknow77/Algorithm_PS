c8e0f95699818ff704307fb644a0d963afd0bc5d
Whoknow77 <gnsdh86166@gmail.com>
2023-02-24 16:12:42 +0900

    feat: inflearn section8 2p

---
+++ inflearn/section8/0812.js
@@ -0,0 +1,27 @@
+// function solution(n, r) {
+//   let answer = [];
+
+//   function DFS(n, r) {
+//     if (n === r || r === 0) return 1;
+//     else return DFS(n - 1, r - 1) + DFS(n - 1, r);
+//   }
+//   answer = DFS(n, r);
+//   return answer;
+// }
+
+// 최적화, 메모이제이션, 전에 사용했던 계산들을 다시하지않고 가져쓴다.
+// 2차원 배열에 저장
+function solution(n, r) {
+  let answer = [];
+  let dy = Array.from(Array(35), () => Array(35).fill(0));
+
+  function DFS(n, r) {
+    if (dy[n][r] > 0) return dy[n][r]; // 이미 구한 값일 경우 사용
+    if (n === r || r === 0) return 1;
+    else return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r)); // 저장후 리턴
+  }
+  answer = DFS(n, r);
+  return answer;
+}
+
+console.log(solution(33, 19));
---
+++ inflearn/section8/0813.js
@@ -0,0 +1,36 @@
+function solution(n, f) {
+  let answer,
+    flag = 0;
+  let dy = Array.from(Array(11), () => Array(11).fill(0)); // 메모이제이션
+  let ch = Array.from({ length: n + 1 }, () => 0); // 순열 돌기위한 check 배열
+  let p = Array.from({ length: n }, () => 0); // 순열 저장
+  let b = Array.from({ length: n }, () => 0); // 조합 저장
+  function combi(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (n === r || r === 0) return 1;
+    else return (dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r));
+  }
+  function DFS(L, sum) {
+    if (L === n && sum === f) {
+      // 순열 완성
+      answer = p.slice();
+    } else {
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === 0) {
+          ch[i] = 1;
+          p[L] = i;
+          DFS(L + 1, sum + b[L] * p[L]);
+          ch[i] = 0;
+        }
+      }
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    b[i] = combi(n - 1, i);
+  }
+
+  DFS(0, 0);
+  return answer;
+}
+
+console.log(solution(4, 16));
