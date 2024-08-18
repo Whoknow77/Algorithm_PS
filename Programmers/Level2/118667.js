3ecde62cb390570538e908a3c6a920b162b9eea4
최훈오 <gnsdh86166@gmail.com>
2024-08-18 18:08:33 +0900

    Solved 1

--- Programmers/Level2/118667.js
+++ Programmers/Level2/118667.js
@@ -1,100 +1,123 @@
-class Node{
-  constructor(data){
-    this.data=data
-    this.next=null
-  }
-}
-class Queue{
-  constructor(){
-    this.head=null
-    this.rear=null
-    this.length=0
-  }
+// class Node{
+//   constructor(data){
+//     this.data=data
+//     this.next=null
+//   }
+// }
+// class Queue{
+//   constructor(){
+//     this.head=null
+//     this.rear=null
+//     this.length=0
+//   }
 
-  enqueue(data){
-    const node = new Node(data)
-    if(!this.head){
-      this.head=node
-    }
-    else{
-      this.rear.next=node
-    }
-    this.rear=node
-    this.length++
-  }
+//   enqueue(data){
+//     const node = new Node(data)
+//     if(!this.head){
+//       this.head=node
+//     }
+//     else{
+//       this.rear.next=node
+//     }
+//     this.rear=node
+//     this.length++
+//   }
 
-  dequeue(){
-    if(!this.head){
-      return false
-    }
-    const data = this.head.data
-    this.head=this.head.next
-    this.length--
-    return data
-  }
-}
+//   dequeue(){
+//     if(!this.head){
+//       return false
+//     }
+//     const data = this.head.data
+//     this.head=this.head.next
+//     this.length--
+//     return data
+//   }
+// }
+
+// function solution(queue1, queue2) {
+
+//   let oneSum = queue1.reduce((prev,cur)=>{
+//       return prev+cur
+//   },0)
+//   let twoSum = queue2.reduce((prev,cur)=>{
+//       return prev+cur
+//   },0)
+
+
+//   const firstQueue = new Queue()
+//   queue1.forEach((item)=>{
+//     firstQueue.enqueue(item)
+//   })
+//   const secondQueue = new Queue()
+//   queue2.forEach((item)=>{
+//     secondQueue.enqueue(item)
+//   })
+  
+//   const halfSum = (oneSum+twoSum)/2 
+  
+//   let count=0
+//   while((oneSum!==0 && twoSum!==0) && count<=600000){
+//       if(oneSum === twoSum){
+//           return count
+//       }
+//       if(oneSum>halfSum){
+//       const removeValue =firstQueue.dequeue()
+//       secondQueue.enqueue(removeValue)
+//        oneSum-=removeValue
+//        twoSum+=removeValue
+//       }
+//       else if(twoSum>halfSum){
+//         const removeValue =secondQueue.dequeue()
+//         firstQueue.enqueue(removeValue)
+//         oneSum+=removeValue
+//         twoSum-=removeValue
+//       }
+//       count+=1
+//   }
+  
+//   return -1
+  
+// }
+
+
+
+
+
+// 다른 풀이(실제로 배열에서 빼지 않고 인덱스로 계산)
 
 function solution(queue1, queue2) {
-
-  let oneSum = queue1.reduce((prev,cur)=>{
-      return prev+cur
-  },0)
-  let twoSum = queue2.reduce((prev,cur)=>{
-      return prev+cur
-  },0)
-
-
-  const firstQueue = new Queue()
-  queue1.forEach((item)=>{
-    firstQueue.enqueue(item)
-  })
-  const secondQueue = new Queue()
-  queue2.forEach((item)=>{
-    secondQueue.enqueue(item)
-  })
+  let answer = 0;
+  let queueSum1 = 0;
+  let queueSum2 = 0;
+  queue1.forEach(element=>queueSum1+=element);
+  queue2.forEach(element=>queueSum2+=element);
+  const totalLen = queue1.length+ queue2.length;
+  let queue1Index = 0;
+  let queue2Index = 0;
   
-  const halfSum = (oneSum+twoSum)/2 
   
-  let count=0
-  while((oneSum!==0 && twoSum!==0) && count<=600000){
-      if(oneSum === twoSum){
-          return count
+  while(queueSum1 !== queueSum2){
+      if(queueSum1>queueSum2){
+          queueSum1 -= queue1[queue1Index];
+          queue2.push(queue1[queue1Index]);
+          queueSum2 +=queue1[queue1Index++];
       }
-      if(oneSum>halfSum){
-      const removeValue =firstQueue.dequeue()
-      secondQueue.enqueue(removeValue)
-       oneSum-=removeValue
-       twoSum+=removeValue
+      else{
+           queueSum1 += queue2[queue2Index];
+           queue1.push(queue2[queue2Index]);
+           queueSum2 -=queue2[queue2Index++];
       }
-      else if(twoSum>halfSum){
-        const removeValue =secondQueue.dequeue()
-        firstQueue.enqueue(removeValue)
-        oneSum+=removeValue
-        twoSum-=removeValue
+       answer++;
+      if(queue1Index+queue2Index>=totalLen){
+          return -1;
       }
-      count+=1
   }
-  
-  return -1
-  
+  return answer;
 }
 
 console.log(solution([1,2,4],[3,2,4]))
 
-// 맨 앞의걸 맨 뒤에 붙임
-
-// dfs로 반복해서 매번 양쪽의 합이 같은지 검사해서 같으면 멈추고 카운트 반환
-
-// 다 돌았는데 합이 다르면 -1 반환
-
-// 둘의 합이 같기전까지 한쪽은 무조건 반값보다크고 한쪽은 반값보다 작다
-
-// 반값보다 큰곳을 기준으로 계속 상대꺼에 붙인다.
-
-// shift땜에 시간복잡도 O(n)오류나는듯 -> O(1)로 바꾸기 위해서 Qeuue또는 dequeue 직접 구현해야함(연결 리스트)
-
-// 서로 스왑하면서 무한반복하는 문제
-
-// 최악의 경우 300000 * n
+// 124
+// 324
 
 
