ae8ec86649c58258dd460eaaab11c5f68fe1390d
최훈오 <gnsdh86166@gmail.com>
2024-09-06 13:37:32 +0900

    Solved 1

--- Programmers/Level2/150368.js
+++ Programmers/Level2/150368.js
@@ -12,6 +12,7 @@ function solution(users, emoticons) {
     cases = temp;
   });
 
+
   cases.forEach(c => {
     let totalPay = 0,
       member = 0;
@@ -29,5 +30,33 @@ function solution(users, emoticons) {
 
   return answer;
 }
+
+
+// dfs 풀이
+
+// function solution(users, emoticons) {
+//   let discount = [10, 20, 30, 40];
+//   let len = emoticons.length;
+//   let res = [];
+//   let arr = Array(len).fill(0);
+
+//   const dfs = (depth) => {
+//       if (depth === len) {
+//           res.push(arr.slice());
+//           return;
+//       }
+
+//       for (let i = 0; i < 4; i++) {
+//           arr[depth] = discount[i];
+//           dfs(depth + 1);
+//           arr[depth] = 0;
+//       }
+//   };
+
+//   dfs(0);
+// }
+
+
+ 
 
 console.log(solution([[40, 10000], [25, 10000]],	[7000, 9000]))
